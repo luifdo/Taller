103ba91d11a61d1b638d7f56fa9275f2aa2d6967
luifdo <luifsanchez@gmail.com>
2025-09-19 18:45:31 -0500

    Add files via upload

---
+++ taller/src/test/java/edu/co/pw/taller/ApplicationTests.java
@@ -0,0 +1,15 @@
+package edu.co.pw.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
